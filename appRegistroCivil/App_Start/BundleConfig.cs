2e2cc47a9c5af31f41fef19994e76bcccf35706f
angeloortega <angeloale1@gmail.com>
2018-08-22 19:17:21 -0600

    Initial commit

--- appRegistroCivil/App_Start/BundleConfig.cs
+++ appRegistroCivil/App_Start/BundleConfig.cs
@@ -20,10 +20,13 @@ namespace appRegistroCivil
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-datepicker.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/bootstrap-datepicker3.css",
                       "~/Content/site.css"));
         }
     }
