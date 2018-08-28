54e53edb9cc91d101c97cf39f3fd7f744bfb1aa3
GUILLIANO D AMBROSIO SOZA <nanodam99@estudiantec.cr>
2018-08-27 23:01:01 -0700

    audio, video, imagen

--- appRegistroCivil/Controllers/PersonasController.cs
+++ appRegistroCivil/Controllers/PersonasController.cs
@@ -7,6 +7,11 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using appRegistroCivil.Models;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Data.SqlClient;
+using System.IO.Pipes;
 
 namespace appRegistroCivil.Views
 {
@@ -19,17 +24,24 @@ namespace appRegistroCivil.Views
         {
             db = TransactionSingletone.db;
             //id = 1;
-            var persona = db.Persona.Include(p => p.Imagenes).Include(p => p.Pais).Include(p => p.Pais1).Include(p => p.Videos).Where(p => p.idPaisResidencia == 1);
+            var persona = db.Persona.Include(p => p.Imagenes).Include(p => p.Pais).Include(p => p.Pais1).Include(p => p.Videos).Where(p => p.idPaisResidencia == 2);
             ViewBag.totalPaises = db.Pais.Count();
             Pais pais = db.Pais.First();
             //Pais pais = db.Pais.Where(x=>x.idPais == id).First();
             ViewBag.idPais = pais.idPais;
             ViewBag.nombrePais = pais.nbrPais;
             ViewBag.area = pais.area;
-            ViewBag.pobActual = pais.poblacionActual;
-            ViewBag.idPresi = pais.idPresidenteActual;
-            ViewBag.audios = pais.himnoNacional;
-            ViewBag.foto = pais.fotoBandera;
+            if (db.Persona.Where(x => x.idPersona == pais.idPresidenteActual).Count() > 0)
+            {
+                ViewBag.nombrePresi = "" + db.Persona.Where(x => x.idPersona == 1 && x.idPaisResidencia == pais.idPais).First().nbrPersona; 
+            }
+            else
+            {
+                ViewBag.nombrePresi = "No hay";
+            }
+            ViewBag.foto = db.Imagenes.Where(x=> x.id == pais.fotoBandera).First().info_bytes;
+            ViewBag.poblacion = db.Persona.Where(x=> x.idPaisResidencia == pais.idPais).Count();
+            ViewBag.himno = db.Audios.Where(x => x.id == pais.himnoNacional).First().info_bytes;
             return View(persona.ToList());
         }
 
@@ -55,16 +67,32 @@ namespace appRegistroCivil.Views
             {
                 id = 1;
             }
+            Pais pais = new Pais();
             var persona = db.Persona.Include(p => p.Imagenes).Include(p => p.Pais).Include(p => p.Pais1).Include(p => p.Videos).Where(p=> p.idPaisResidencia == id);
-            Pais pais = db.Pais.Where(x=>x.idPais == id).First();
+            
+            if (db.Pais.Where(x => x.idPais == id).Count() > 0)
+            {
+                pais = db.Pais.Where(x => x.idPais == id).First();
+            }
+            else
+            {
+                return PaisIndex(id + 1);
+            }
             ViewBag.totalPaises = db.Pais.Count();
             ViewBag.nombrePais = pais.nbrPais;
             ViewBag.area = pais.area;
-            ViewBag.pobActual = pais.poblacionActual;
-            ViewBag.idPresi = pais.idPresidenteActual;
-            ViewBag.audios = pais.himnoNacional;
-            ViewBag.foto = pais.fotoBandera;
+            if (db.Persona.Where(x => x.idPersona == pais.idPresidenteActual).Count() > 0)
+            {
+                ViewBag.nombrePresi = "" + db.Persona.Where(x => x.idPersona == pais.idPresidenteActual && x.idPaisResidencia == pais.idPais).First().nbrPersona + ""; 
+            }
+            else
+            {
+                ViewBag.nombrePresi = "No hay";
+            }
+            ViewBag.foto = db.Imagenes.Where(x => x.id == pais.fotoBandera).First().info_bytes;
             ViewBag.idPais = pais.idPais;
+            ViewBag.poblacion = db.Persona.Where(x => x.idPaisResidencia == pais.idPais).Count();
+            ViewBag.himno = db.Audios.Where(x => x.id == pais.himnoNacional).First().info_bytes;
             return View("Index", persona.ToList());
         }
 
@@ -122,8 +150,11 @@ namespace appRegistroCivil.Views
         // GET: Pais/Create
         public ActionResult CreatePais()
         {
-            ViewBag.himnoNacional = new SelectList(db.Audios, "id", "descripcion");
-            ViewBag.fotoBandera = new SelectList(db.Imagenes, "id", "descripcion");
+            
+            db = TransactionSingletone.db;
+            ViewBag.fotoBandera = db.Imagenes.Where(x => x.id == 0).First().info_bytes;
+            //ViewBag.himno = new SelectList(db.Audios, "id", "descripcion");
+            //ViewBag.fotoBandera = db.Imagenes.Where(x => x.id == 0);
             return View();
         }
 
@@ -132,18 +163,32 @@ namespace appRegistroCivil.Views
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult CreatePais([Bind(Include = "idPais,nbrPais,area,poblacionActual,fotoBandera,himnoNacional,idPresidenteActual")] Pais pais)
+        public ActionResult CreatePais([Bind(Include = "idPais,nbrPais,area,poblacionActual,fotoBandera,himnoNacional,idPresidenteActual")] Pais pais, HttpPostedFileBase file)
         {
-            if (ModelState.IsValid)
+            //Image convert = Image.FromFile("C:\\Users\\Giulliano\\Desktop\\himno.mp4");
+            //ImageConverter _imageConverter = new ImageConverter();
+            //byte[] xByte = (byte[])_imageConverter.ConvertTo(convert, typeof(byte[]));
+            Videos imageI = new Videos();
+            byte[] audiobyte = System.IO.File.ReadAllBytes("C:\\Users\\Giulliano\\Desktop\\interview.mp4");
+            //imageI.id = db.Imagenes.Count();
+            imageI.id = 2;
+            //imageI.descripcion = ""+ pais.nbrPais + "";
+            imageI.descripcion = "Default Interview";
+            imageI.info_bytes = audiobyte;
+            pais.fotoBandera = imageI.id;
+            try
             {
-                db.Pais.Add(pais);
+                db.Videos.Add(imageI);
+                //db.Pais.Add(pais);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            ViewBag.himnoNacional = new SelectList(db.Audios, "id", "descripcion", pais.himnoNacional);
-            ViewBag.fotoBandera = new SelectList(db.Imagenes, "id", "descripcion", pais.fotoBandera);
-            return View(pais);
+            catch
+            {
+                ViewBag.himnoNacional = new SelectList(db.Audios, "id", "descripcion", pais.himnoNacional);
+                ViewBag.fotoBandera = new SelectList(db.Imagenes, "id", "descripcion", pais.fotoBandera);
+                return View(pais);
+            }
         }
 
         // GET: Personas/Edit/5
@@ -174,6 +219,7 @@ namespace appRegistroCivil.Views
         {
             if (ModelState.IsValid)
             {
+                db = new RegistroCivilEntities();
                 db.Entry(persona).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
